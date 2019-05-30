990633f79862b4df233c3d4c11af6e00887f27cf
Jorge Saez L <araneldon@hotmail.com>
2019-05-29 23:00:16 -0400

    inicial

---
+++ Js.Business/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Js.Business
+{
+    public class Context : DbContext
+    {
+        public Context() : base("ProductsDB") { }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Js.Business/ServicioProducto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Js.Business
+{
+    public class ServicioProducto
+    {
+
+        public void Ingresa(string name, decimal precionormal, decimal preciointernet)
+        {
+            var product = new Product { Name = name, PrecioInternet = preciointernet, PrecioNormal = precionormal };
+
+            using (var ctx = new Context())
+            {
+                ctx.Products.Add(product);
+                ctx.SaveChanges();
+            }
+
+        }
+
+    }
+}
